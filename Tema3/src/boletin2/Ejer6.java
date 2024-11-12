a0e1bf5d9195877c4e151080804e515e491de845
ruben-carrasco <ruben.carrasco@iesnervion.es>
2024-11-12 21:52:50 +0100

    Ejer 6 y 7

--- Tema3/src/boletin2/Ejer6.java
+++ Tema3/src/boletin2/Ejer6.java
@@ -1,21 +1,49 @@
 package boletin2;
 
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ejer6 {
 	public static void main(String[] args) {
+		// Array de la primitiva generada
 		int primitiva[] = new int[6];
-		int ganadora[] = new int[6];
+		// Array de la primitiva ganadora
+		int ganadora[] = {21, 32, 12, 5, 28, 8};
+		// Contador de aciertos
+		int aciertos = 0;
 		
+		// Creamos el Scanner y el Random
 		Scanner reader = new Scanner(System.in);
 		Random rand = new Random();
 		
-		// Combinacion ganadora
-		for (int i = 0; i < ganadora.length; i++) {
-			
+		// Ordenamos la combinacion ganadora
+		Arrays.sort(ganadora);
+		
+		// Mostramos la combinacion ganadora
+		System.out.println("La combinacion ganadora es: " + Arrays.toString(ganadora));
+		
+		// Creacion primitiva
+		for (int i = 0; i < primitiva.length; i++) {
+			primitiva[i] = rand.nextInt(1,50);
 		}
+		
+		// Mostramos la primitiva
+		System.out.println("Tu primitiva es: " + Arrays.toString(primitiva));
+		
+		// Contamos los aciertos
+		for (int i = 0; i < primitiva.length; i++) {
+			if (primitiva[i] == ganadora[i]) {
+				aciertos++;
+			}
+		}
+		
+		// Mostramos el numero de aciertos
+		System.out.println("Tienes " + aciertos + " aciertos");
+		
+		// Cerramos el Scanner
+		reader.close();
 	}
 
 }
--- Tema3/src/boletin2/Ejer7.java
+++ Tema3/src/boletin2/Ejer7.java
@@ -1,5 +1,29 @@
 package boletin2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejer7 {
+	public static void main(String[] args) {
+		// Tabla del array
+		int tabla[] = new int[55];
+		// Posicion
+		int index = 0;
+		
+		// Creamos Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Iteracion para cada numero
+		for (int i = 1; i <= 10; i++) {
+				Arrays.fill(tabla, index, index + i, i);
+				index += i;
+			}
+		
+		// Mostramos resultado
+		System.out.println(Arrays.toString(tabla));
+		
+		// Cerramos Scanner
+		reader.close();
+	}
 
 }
