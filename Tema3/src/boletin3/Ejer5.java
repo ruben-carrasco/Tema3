8dd697519fd1f3b381e41998814c6db6a3254731
ruben-carrasco <ruben.carrasco@iesnervion.es>
2024-11-13 10:02:32 +0100

    5

--- Tema3/src/boletin3/Ejer5.java
+++ Tema3/src/boletin3/Ejer5.java
@@ -1,7 +1,19 @@
 package boletin3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejer5 {
 	public static void main(String[] args) {
+		// Creamos el Array
+		int tabla[][] = new int[4][5];
+		
+		// Creamos el Scanner y el Random
+		Scanner reader = new Scanner(System.in);
+		Random rand = new Random();
+		
+		// Cerramos el Scanner
+		reader.close();
 		
 	}
 
